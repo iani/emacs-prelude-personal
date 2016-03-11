c55fe79c674702771769a1194587e781458f630d
iani <zannos@gmail.com>
2016-03-11 09:58:47 +0200

    greek input

--- postload/026_greek_input.el
+++ postload/026_greek_input.el
@@ -1,3 +1,4 @@
 ;; (global-set-key (kbd "C-c C-\\") 'toggle-input-method)
-(global-set-key (kbd "s-;") 'toggle-input-method)
 
+(setq default-input-method "greek")
+(global-set-key (kbd "s-;") 'toggle-input-method)
