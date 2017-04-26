d4f7d6149cff9fc0623106e77dd001038d2fefa7
Ioannis Zannos <iani@chamade.avarts.ionio.gr>
2017-04-26 10:24:10 +0300

    +fn-ace-keybindings

---
+++ postload/022_fn_ace_keybindings.el
@@ -0,0 +1,6 @@
+;; alternative keybindings needed to use ace-jump with greek input mode
+;; 26 Apr 2017 10:19
+
+(global-set-key (kbd "h-j") 'ace-jump-word-mode)
+(global-set-key (kbd "h-k") 'ace-jump-char-mode)
+(global-set-key (kbd "h-l") 'ace-jump-line-mode)
