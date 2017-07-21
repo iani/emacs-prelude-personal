86fffac7e8677bdceb6d6550e12b526dcd18450f
iani <zannos@gmail.com>
2017-07-21 21:20:16 +0900

    developing untangle-tangle.el

--- postload/006_untangle_tangle.el
+++ postload/006_untangle_tangle.el
@@ -16,18 +16,51 @@
        (files (file-expand-wildcards (concat directory "*.el"))))
     ;; (message (concat (file-truename directory) filename))
     (find-file filename)
-    (mapc (lambda (fname)
-            (message fname))
+    (erase-buffer)
+    (mapc 'org-el-insert-1-file
           files)
-    ;; (erase-buffer)
     
     )
-)
-
-(defun org-el-tangle-sections ()
-  "Export each sections' emacs-lisp block to a separate file."
-  (interactive)
   )
+
+(defun org-el-insert-1-file (fname)
+  "insert file FNAME into the master org file.
+Create org header and SRC block from data in FNAME file."
+  (message fname)
+  (save-excursion
+    (let*
+        ((fname-base (file-name-base fname))
+         (header (concat
+                  "\n* "
+                  fname-base))
+         header-found)
+      (find-file fname)
+      (goto-char (point-min))
+      (setq header-found
+            (search-forward
+             fname-base
+             (line-end-position 1)
+             t
+             1))
+      (cond
+       (header-found
+        (message "a header WAS found")
+        (message "the line containing the header is:")
+        (message (buffer-substring (line-beginning-position)
+                                   (line-end-position 1))))
+       (t
+        (message "a header was NOTTTTTTTTT found")
+        (message "the first line is:")
+        (message (buffer-substring (line-beginning-position)
+                                   (line-end-position 1)))
+        ))
+      ))
+  )
+
+( org-el-tangle-sections ()
+                         "Export each sections' emacs-lisp block to a separate file."
+                         (interactive)
+                         )
 
 (format-time-string "%y%m%d")
 
