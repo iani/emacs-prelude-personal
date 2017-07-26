975ee021c44c9db657cacd1bc8801458edc1b230
iani <zannos@gmail.com>
2017-07-26 20:52:58 +0900

    perspective removed

--- postload/019_projectile.el
+++ postload/019_projectile.el
@@ -4,11 +4,12 @@
   ;;; helm-projectile, helm-perspective
 
   ;;; Code:
-  (prelude-load-require-packages '(perspective helm-projectile persp-projectile))
+;; (prelude-load-require-packages '(perspective helm-projectile persp-projectile))
+(prelude-load-require-packages '(helm-projectile))
   (helm-projectile-on)
   (setq projectile-switch-project-action #'projectile-commander)
-  (persp-mode)
-  (require 'persp-projectile)
-  (define-key projectile-mode-map (kbd "s-s") 'projectile-persp-switch-project)
+  ;; (persp-mode)
+  ;; (require 'persp-projectile)
+  ;; (define-key projectile-mode-map (kbd "s-s") 'projectile-persp-switch-project)
 (provide 'projectile)
 ;;; 019_projectile.el ends here
