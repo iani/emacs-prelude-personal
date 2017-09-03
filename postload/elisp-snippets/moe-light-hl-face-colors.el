ea4e15b5f887723d9defdf544fe02468d77f35e9
iani <zannos@gmail.com>
2017-09-03 13:07:34 +0900

    e-lisp snippet: moe-light faces

---
+++ postload/elisp-snippets/moe-light-hl-face-colors.el
@@ -0,0 +1,13 @@
+;;; Some useful face color tweaks.
+;;; Mainly: Making region, line and sexp higlights visible with high contrast
+;;; for dark or for light color themes.
+
+;;; dark blue is more visible for selected region color in moe-dark or zenburn
+;;; seashell is better for moe-light
+;;; than the default color in zenburn or moe-dark
+
+(custom-set-faces
+ '(region ((t (:background "seashell"))))
+ ;; following 2 are for moe-light:
+ '(hl-line ((t (:background "papaya whip" :foreground "pale violet red"))))
+ '(hl-sexp-face ((t (:background "linen" :foreground "VioletRed4")))))
