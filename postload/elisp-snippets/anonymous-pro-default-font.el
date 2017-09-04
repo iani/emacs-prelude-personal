af60c522946c8c900216c5a1b35c822dab50caff
iani <zannos@gmail.com>
2017-09-05 08:46:07 +0900

    anonymous font + sc-c clear post buffer

---
+++ postload/elisp-snippets/anonymous-pro-default-font.el
@@ -0,0 +1,12 @@
+;;; experimental
+
+(custom-set-faces
+ '(default ((t (:family "Anonymous Pro for Powerline" :foundry "nil" :width normal :height 120
+                        :weight normal :slant normal :underline nil :overline nil
+                        :strike-through nil :box nil :inverse-video nil :foreground "#DCDCCC"
+                        :background "#3F3F3F" :stipple nil :inherit nil))))
+ )
+
+;; (custom-set-faces
+;;  '(default ((t (:family "Inconsolata" :foundry "nil" :width normal :height 140 :weight normal :slant normal :underline nil :overline nil :strike-through nil :box nil :inverse-video nil :foreground "#DCDCCC" :background "#3F3F3F" :stipple nil :inherit nil))))
+;;  )
