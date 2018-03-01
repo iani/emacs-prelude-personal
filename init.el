20dec7b531f5e6eabebfbc8764a61c3979524673
iani <zannos@gmail.com>
2018-03-01 17:56:32 +0200

    +postload-extras for user customization

--- init.el
+++ init.el
@@ -14,5 +14,14 @@
           (load-file path))
         (file-expand-wildcards "~/.emacs.d/personal/postload/*.el"))
 
+;;; Last load an additional folder including user modifications
+;;; This folder is ignored by the present git repository.
+;;; A user may either put the code directly in the folder,
+;;; or create a symbolic link to this folder from a folder containing his own code.
+;;; The second option allows the user to maintain the code to a separate git repository.
+(mapcar (lambda (path)
+          (load-file path))
+        (file-expand-wildcards "~/.emacs.d/personal/postload-extras/*.el"))
+
 (provide 'init.el)
 ;;; init.el ends here
